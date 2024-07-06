f5904f5e8e5eccee2690a4d113b636c1ab84d39b
Tushar Mankar <tusharmankar02@gmail.com>
2024-07-06 17:19:52 +0530

    update code

--- main.py
+++ main.py
@@ -20,33 +20,27 @@ class clsQrGen(QMainWindow):
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=5,
-            border=2,
+            box_size=10,
+            border=4,
         )
         qr.add_data(txt)
         qr.make(fit=True)
 
-
-        self.img = qr.make_image(fill='black', back_color='white')
-
-        self.filName = f'{self.ui.txtBuisName.text()}.png'
-        self.img.save(self.filName)
-        pixmap = QPixmap(self.filName)
-        pixmap.scaledToWidth(250)
-        pixmap.scaledToHeight(250)
-        self.ui.labQrShow.setPixmap(pixmap)
+        img = qr.make_image(fill='black', back_color='white')
+        img.save('business_card_qr_with_location.png')
+        print("QR code with location generated and saved as 'business_card_qr_with_location.png'")
 
     def btnGenEvent(self):
-        self.txt = f"""BEGIN:VCARD
-        VERSION:3.0
-        FN:{self.ui.txtName.text()}
-        ORG:{self.ui.txtBuisName.text()}
-        TITLE:{self.ui.txtTitel.text()}
-        TEL;TYPE=WORK,VOICE:+91 {self.ui.txtContact.text()}
-        ADR;TYPE=WORK:;;{self.ui.txtAddress.text()}
-        EMAIL:{self.ui.txtEmail.text()}
-        URL:{self.ui.txtUrl.text()}
-        END:VCARD"""
+        # self.txt = f"""BEGIN:VCARD
+        # VERSION:3.0
+        # FN:{self.ui.txtName.text()}
+        # ORG:{self.ui.txtBuisName.text()}
+        # TITLE:{self.ui.txtTitel.text()}
+        # TEL;TYPE=WORK,VOICE:+91 {self.ui.txtContact.text()}
+        # ADR;TYPE=WORK:;;{self.ui.txtAddress.text()}
+        # EMAIL:{self.ui.txtEmail.text()}
+        # URL:{self.ui.txtUrl.text()}
+        # END:VCARD"""
         self.txt = """BEGIN:VCARD
         VERSION:3.0
         FN:Paresh Jain
